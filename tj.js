e58e4034f3df14ec0181b2702e436a7cca69cdfa
whuanle <1586052146@qq.com>
2024-02-18 19:07:42 +0800

    first commit

---
+++ tj.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?22de128f5a7d2d5b218af6b9e6e37e4c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
